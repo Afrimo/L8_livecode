43ce1a934cb441e9c89ad52489bad65aa39d20fe
Afrimo <Afrimthaqi1401@yahoo.no>
2023-07-03 15:31:00 +0200

    Step 10 - New Movie frontend

--- server/moviesApi.js
+++ server/moviesApi.js
@@ -1,15 +1,17 @@
 import express from "express";
 
 export const moviesApi = express.Router();
+
+const movies  = [
+    {
+        title: "Rise of the planet of the apes"
+    },
+    {
+        title: "Sangre por sangre"
+    }
+]
 moviesApi.get("/", (req, res) => {
-    res.json([
-        {
-            title: "Rise of the planet of the apes"
-        },
-        {
-            title: "Sangre por sangre"
-        }
-    ])
+    res.json(movies)
 })
 
 
