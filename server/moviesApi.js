f3e023d97bfcafdf9f25aed8f012091007792276
Afrimo <Afrimthaqi1401@yahoo.no>
2023-07-17 21:53:32 +0200

    Step 11 - Get movies from DB [fixed]

--- server/moviesApi.js
+++ server/moviesApi.js
@@ -1,18 +1,5 @@
 import express from "express";
 
-const movies  = [
-    {
-        title: "Rise of the planet of the apes",
-        year: "2011",
-        plot: "Some niggas fighting over territory"
-    },
-    {
-        title: "Sangre por sangre",
-        year: "1987",
-        plot: "mexican gang violence and shit"
-    }
-]
-
 export function MoviesApi(db){
     const api = express.Router();
 
@@ -23,12 +10,19 @@ export function MoviesApi(db){
         const collection = db.collection("movies")
         console.log(collection)
 
+        const movies = await db
+            .collection("movies")
+            .find({})
+            .map(({ title, year, plot }) => ({ title, year, plot }))
+            .toArray();
+
+        res.json(movies)
     })
 
     api.post("/", (req, res) => {
         const {title, year, plot} = req.body;
 
-        movies.push( {title, year, plot})
+        db.collection("movies").insertOne({ title, year, plot })
 
         res.sendStatus(284)
     })
